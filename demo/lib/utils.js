d3baabd857be322409e443fa7335cd8363b6be15
Ryan <ryblas@proton.me>
2025-08-02 12:43:18 -0500

    Update Rocket League demo for latest auth and WebSocket protocol (#54)
    * Update demo for latest Rocket League version with new auth route and websocket protocol
    
    * Update build id and feature set

--- demo/lib/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    bufferToHex: function (buffer) {
-        return Array
-            .from(new Uint8Array(buffer))
-            .map(b => b.toString(16).padStart(2, "0"))
-            .join("");
-    }
-};
